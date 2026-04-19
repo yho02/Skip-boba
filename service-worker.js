10f799a55370b16bc5a2553995d58cf84ad52b3e
yho02 <120296746+yho02@users.noreply.github.com>
2026-04-18 22:27:43 -0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE = 'boba-savings-v1';
+const FILES = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
